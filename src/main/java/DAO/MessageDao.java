a67c43506fa719e67285a67caf8e183ad5b90e9c
Zhou Liu <zhouliu2111@gmail.com>
2025-06-10 19:16:55 +0000

    Create MessageDao.java

---
+++ src/main/java/DAO/MessageDao.java
@@ -0,0 +1,27 @@
+package DAO;
+
+import Model.Message;
+import java.util.ArrayList;
+
+public class MessageDao {
+    
+    public Message insertMessage (Message message) {
+        return null;
+    }
+
+    public ArrayList<Message> getAllMessages() {
+        return new ArrayList<>();
+    }
+
+    public Message getMessageById(int id) {
+        return null;
+    }
+
+    public Message updateMessage(int id, String newText) {
+        return null;
+    }
+
+    public ArrayList<Message> getMessageByUserId(int userId) {
+        return new ArrayList<>();
+    }
+}
