be9d1a74eb818034b38cddf23345092991fd39dd
Zhou Liu <zhouliu2111@gmail.com>
2025-06-10 19:20:20 +0000

    create AccountService.java

---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,17 @@
+package Service;
+
+import DAO.AccountDao;
+import Model.Account;
+
+public class AccountService {
+    
+    AccountDao accountDao = new AccountDao();
+
+    public Account register(Account account) {
+        return null;
+    }
+
+    public Account login(String un, String pw) {
+        return null;
+    }
+}
