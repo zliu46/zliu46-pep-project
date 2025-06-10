80ef1b7d27b64a77f962c033107842b03c979d47
Zhou Liu <zhouliu2111@gmail.com>
2025-06-10 19:11:13 +0000

    Update methods in AccountDao.java

--- src/main/java/DAO/AccountDao.java
+++ src/main/java/DAO/AccountDao.java
@@ -6,5 +6,20 @@ import java.sql.*;
 
 public class AccountDao {
     
-    
+    public Account insertAccount (Account account) {
+
+        return null;
+    }
+
+    public Account getAccountByUsernameAndPassword (String un, String pw) {
+        return null;
+    }
+
+    public boolean isUsernameTaken(String un) {
+        return false;
+    }
+
+    public boolean isUsernameTakenById (int id) {
+        return false;
+    }
 }
