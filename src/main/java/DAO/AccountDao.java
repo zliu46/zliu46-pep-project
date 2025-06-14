ca804efc494261de4ef06c8f151278c258cfb89a
Zhou Liu <zhouliu2111@gmail.com>
2025-06-14 01:16:56 +0000

    Implemented logic to insertAccount method in AccountDao

--- src/main/java/DAO/AccountDao.java
+++ src/main/java/DAO/AccountDao.java
@@ -6,8 +6,21 @@ import java.sql.*;
 
 public class AccountDao {
     
-    public Account insertAccount (Account account) {
-        
+    public Account insertAccount (Account account) throws SQLException {
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            String sql = "INSERT INTO account (username. password) VALUES (?, ?)";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, account.getUsername());
+            ps.setString(2, account.getPassword());
+            ps.executeUpdate();
+            ResultSet keys = ps.getGeneratedKeys();
+            if (keys.next()) {
+                int generatedId = keys.getInt(1);
+                return new Account (generatedId, account.getUsername(), account.getPassword());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
