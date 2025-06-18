59454b95c9bb8ecfbb6de9c8852a0febaf57f075
Zhou Liu <zhouliu2111@gmail.com>
2025-06-18 22:16:11 +0000

    Updated createMessage method in MessageService.java

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -3,14 +3,25 @@ package Service;
 import DAO.AccountDao;
 import DAO.MessageDao;
 import Model.Message;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public class MessageService {
     MessageDao messageDao = new MessageDao();
     AccountDao accountDao = new AccountDao();
     
-    public Message createMessage(Message message) {
-        return null;
+    public Message createMessage(Message message) throws SQLException {
+        if (message.getMessage_text() == null || message.getMessage_text().isBlank() ||
+                message.getMessage_text().length() > 255 ) {
+                    return null;
+        }
+
+        if (!accountDao.isUsernameTakenById(message.getPosted_by())) {
+            return null;
+        }
+
+        return messageDao.insertMessage(message);
     }
 
     public List<Message> getAllMessages() {
